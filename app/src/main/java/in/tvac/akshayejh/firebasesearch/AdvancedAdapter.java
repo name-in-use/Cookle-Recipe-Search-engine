34f286a76b9387d4f433b4d0c9ee1c4f6ff48638
name-in-use <xrhstoskr@outlook.com>
2020-04-23 22:12:07 +0300

    cooment

--- app/src/main/java/in/tvac/akshayejh/firebasesearch/AdvancedAdapter.java
+++ app/src/main/java/in/tvac/akshayejh/firebasesearch/AdvancedAdapter.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
+//comment
 public class AdvancedAdapter extends RecyclerView.Adapter<AdvancedAdapter.AdvancedViewHolder> {
     private ArrayList<AdvancedItem> mAdvancedList;
     private OnItemClickListener mListener;
